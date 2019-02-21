2442920320513f6197ed5c8ff8dddac7e6080c61
vaibnak <vaibnakgupta100@gmail.com>
2019-02-21 20:39:36 +0530

    activity after login made

--- app/src/main/java/com/example/user/picshare/MainActivity.java
+++ app/src/main/java/com/example/user/picshare/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.picshare;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,13 +17,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnKeyListener, View.OnClickListener {
     EditText editText1;
     EditText editText2;
+    EditText editText3;
     Button sgin;
     Button sgup;
     private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +36,12 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         backgroundRelativeLayout.setOnClickListener(this);
         editText1 = (EditText)findViewById(R.id.email);
         editText2 = (EditText)findViewById(R.id.pass);
+        editText3 = (EditText)findViewById(R.id.name);
         editText2.setOnKeyListener(this);
         sgin = (Button)findViewById(R.id.button1);
         sgup = (Button)findViewById(R.id.button2);
         mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
     }
 
 
@@ -52,15 +59,27 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
     }
 
+    public void putinDatabase(String name, String email){
+        String id = databaseReference.push().getKey();
+        users item = new users(name,email);
+        databaseReference.child(id).setValue(item);
+        Intent intent = new Intent(getApplicationContext(), showUsers.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+    }
+
     public void signup(View view) {
-        String email = editText1.getText().toString();
+       final String email = editText1.getText().toString();
         String pass = editText2.getText().toString();
+       final String name = editText3.getText().toString();
         mAuth.createUserWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            putinDatabase(name, email);
                             Toast.makeText(getApplicationContext(), "registration succesfull", Toast.LENGTH_SHORT).show();
+
                         }else{
                             Toast.makeText(getApplicationContext(), "registration unsuccesfull", Toast.LENGTH_SHORT).show();
                         }
@@ -71,14 +90,16 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
     public void signin(View view) {
         String email = editText1.getText().toString();
         String pass = editText2.getText().toString();
+        final String name = editText3.getText().toString();
         mAuth.signInWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(getApplicationContext(), "signin succesfull", Toast.LENGTH_SHORT).show();
-//                            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-//                            startActivity(intent);
+                            Intent intent = new Intent(getApplicationContext(), showUsers.class);
+                            intent.putExtra("name", name);
+                            startActivity(intent);
                         }else{
                             Toast.makeText(getApplicationContext(), "signin Unsuccessful", Toast.LENGTH_SHORT).show();
                         }
